8537b8eaf83785db79c319b0490c4fb9d6a94bea
Saeed <saeedag.imail@gmail.com>
2024-07-25 16:56:07 +0200

    bugfix

--- racetracer/messenger_app/views.py
+++ racetracer/messenger_app/views.py
@@ -7,7 +7,7 @@ from .models import Message, Tag, TestSession
 from .serializers import MessageSerializer, TagSerializer, TestSessionSerializer, RegisterSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework import generics
+from rest_framework import generics 
 from django.contrib.auth.models import User
 
 
