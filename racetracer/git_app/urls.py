89d8b71678aa7216a9837b0dd11fd9e54f66a204
GETracing <saeed.allgharaee003@stud.fh-dortmund.de>
2024-08-28 18:15:40 +0200

    Fixed thr git issue. added config file.

--- racetracer/git_app/urls.py
+++ racetracer/git_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('add/', views.git_add, name='git add'),
-    path('commit/', views.git_commit, name="git commit"),
+    
     path('diff/', views.git_diff, name='git diff'),
     path('push/', views.git_add_commit_push, name='git push'),
+    path('setup/', views.set_configurations,name='setup'),
 ]
